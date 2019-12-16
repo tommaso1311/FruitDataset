00285e2d191b35c59635088fe1b9f1c7c5bcbd32
tommaso <tommaso131195@gmail.com>
2019-12-16 12:47:37 +0100

    Create temp function that returns image array

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,14 @@
 from source.spherical_fruit import SphericalFruit
+import matplotlib.pyplot as plt
 
 fruit = SphericalFruit()
-fruit.rotate_pixels_ll(5, 5)
+# fruit.rotate_pixels_ll(5, 5)
+fruit.generate_defects([(5, 10)])
+
+arr, clusters = fruit.temp()
+
+print(arr.shape, clusters)
+
+fig, ax = plt.subplots(figsize=(4, 4))
+plt.imshow(arr)
+plt.show()
--- source/spherical_fruit.py
+++ source/spherical_fruit.py
@@ -1,6 +1,8 @@
 from math import pi, sqrt, cos, sin
 from source.utils import rotation_ll
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors
 
 class SphericalFruit():
 	"""docstring for SphericalFruit"""
@@ -44,3 +46,62 @@ class SphericalFruit():
 		point = np.array([0, 0, 0])
 		points = rotation_ll(self.pixels_coordinates, point, lon_angle, lat_angle)
 		return points
+
+	def generate_defects_coordinates(self, min_defects, max_defects):
+
+		n_defects = np.random.randint(min_defects, max_defects)
+		idx = np.random.choice(self.pixels_coordinates.shape[0], n_defects, replace=False)
+		defects_coordinates = self.pixels_coordinates[idx, :]
+		return defects_coordinates
+
+	def add_defects(self, defect_type=1, min_defects=1, max_defects=5):
+
+		defects_coordinates = self.generate_defects_coordinates(min_defects, max_defects)
+
+		for defect in defects_coordinates:
+
+			self.n_defects += 1
+
+			defects_distances = np.linalg.norm(defects_coordinates-defect, axis=1)
+			points_distances = np.linalg.norm(self.pixels_coordinates-defect, axis=1)
+
+			max_radius = np.min(defects_distances[np.nonzero(defects_distances)])/2 if (len(defects_distances)>1) else 0.3
+			radius = np.random.random()*max_radius
+			mask = points_distances < radius
+
+			self.pixels_values[mask] = defect_type
+			self.pixels_cluster[mask] = self.n_defects
+
+	def generate_defects(self, defects_list=[(1, 2)]):
+
+		for defect_type, min_max in enumerate(defects_list, 1):
+			self.add_defects(defect_type, min_max[0], min_max[1])
+
+	def temp(self, size=(4, 4), pov=(0, 0)):
+
+		fig, ax = plt.subplots(figsize=size)
+
+		rotated_pixels_coordinates = self.rotate_pixels_ll(pov[0], pov[1])
+
+		mask = rotated_pixels_coordinates[:, 2]<0
+		projected_pixels_coordinates = rotated_pixels_coordinates[mask]
+		projected_pixels_values = self.pixels_values[mask]
+		projected_pixels_cluster = self.pixels_cluster[mask]
+
+		clusters = np.unique(projected_pixels_cluster)
+
+		for i, c in enumerate(self.colors):
+			mask = np.where(projected_pixels_values==i)
+			ax.scatter(projected_pixels_coordinates[mask][:,0],
+						projected_pixels_coordinates[mask][:,1], color=c, marker='.')
+
+		plt.axis('off')
+		plt.gca().invert_yaxis()
+		plt.margins(0,0)
+
+		fig.canvas.draw()
+
+		fig_array = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+		fig_array = fig_array.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+		return fig_array, clusters
